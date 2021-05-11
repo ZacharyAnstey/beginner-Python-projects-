69620f7be4ac4ddf9f96a0586210cb802dbfceea
ZacharyAnstey <77645692+ZacharyAnstey@users.noreply.github.com>
2021-05-11 12:43:52 -0230

    Add files via upload

---
+++ 10_random_numbers.py
@@ -0,0 +1,6 @@
+# This program prints 10 random numbers each on a new line. 
+
+from random import random # Use the random libary to generate random number
+for i in range(10):
+	value = random()
+	print(value)
