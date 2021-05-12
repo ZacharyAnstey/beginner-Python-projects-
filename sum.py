2eb02cebce2be32f8876f6b10f7b023a40ac4ab5
Zachary Anstey <77645692+ZacharyAnstey@users.noreply.github.com>
2021-05-12 08:51:04 -0230

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,7 @@
+"""
+This program takes two numbers as input and then finds the sum of theses two numbers
+"""
+num1 = int(input('Enter a number'))
+num2 = int(input('Enter a second number'))
+sum = num1 + num2
+print('the sum of',num1,'and',num2,'is',sum)
