9126819d0e4d7b119e544e1642b86eb8fbd14eb4
Zachary Anstey <77645692+ZacharyAnstey@users.noreply.github.com>
2021-05-13 15:40:25 -0230

    Add files via upload

---
+++ shape.py
@@ -0,0 +1,8 @@
+# This program computes if a shape is a square or a rectangle 
+side1 = int(input('Enter the length of one side of your object'))
+side2 = int(input('Enter the length of a second side of your object'))
+
+if side1 == side2:
+    print('Your object is a square')
+else:
+    print('Your object is a rectangle')
